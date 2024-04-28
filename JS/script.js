899ffe4ffc27b8869771b35fad88b79fd0eb6852
ihshadin <ihshadin3453@gmail.com>
2024-04-29 04:37:01 +0600

    copy function, user icon toggle fix, make input style

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+// Output Depth & Length
 const outputDepth = document.getElementById("output-depth");
 const outputDepthValue = document.getElementById("output-depth-value");
 
@@ -6,4 +7,34 @@ outputDepth.addEventListener("input", function () {
   outputDepth.style.setProperty("--range-value", outputDepth.value + "%");
   outputDepthValue.textContent = outputDepth.value;
   outputDepthValue.style.left = outputDepth.value + "%";
+});
+
+// User Icon Dropdown hide when click user icon
+const userIconBtn = document.getElementById("user_icon_btn");
+const userInfo = document.getElementById("user_info_container");
+
+userIconBtn.addEventListener("click", () => {
+  userInfo.classList.toggle("!block");
+});
+window.addEventListener("click", (e) => {
+  if (!userIconBtn.contains(e.target)) {
+    userInfo.classList.remove("!block");
+  }
+});
+
+// Text copy
+const copyTextContent = document.getElementById("copy_text_content");
+const copyTextSuccess = document.getElementById("copy_text_success");
+const copyTextBtn = document.getElementById("copy_text_btn");
+
+copyTextBtn.addEventListener("click", () => {
+  const range = document.createRange();
+  range.selectNode(copyTextContent);
+  window.getSelection().addRange(range);
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();
+  copyTextSuccess.classList.add("!block");
+  setTimeout(() => {
+    copyTextSuccess.classList.remove("!block");
+  }, 2000);
 });
